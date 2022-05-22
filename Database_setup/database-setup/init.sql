177d985ec98a2705326bef4bd2815807b3b521df
Monica Pricope <pricopemonica12@yahoo.com>
2022-05-22 20:40:47 +0300

    updated jenkins + init script

--- Database_setup/database-setup/init.sql
+++ Database_setup/database-setup/init.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE IF NOT EXISTS test;
 
 use test;
-CREATE TABLE test.shelter (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(20) NOT NULL, location VARCHAR(20) NOT NULL, admin_name VARCHAR(25) NOT NULL, admin_phone VARCHAR(15) NOT NULL, ocupants INT NOT NULL, capacity INT NOT NULL, PRIMARY KEY(id)) ENGINE=InnoDB;
+
+CREATE TABLE test.shelter ( id INT NOT NULL AUTO_INCREMENT , title VARCHAR(20) NOT NULL , location VARCHAR(20) NOT NULL , admin_name VARCHAR(25) NOT NULL , admin_phone VARCHAR(15) NOT NULL , ocupants INT NOT NULL , capacity INT NOT NULL , user_id INT NOT NULL , PRIMARY KEY (id)) ENGINE = InnoDB;
+CREATE TABLE test.user2 ( id INT NOT NULL AUTO_INCREMENT , username VARCHAR(20) NOT NULL , password VARCHAR(20) NOT NULL , name VARCHAR(25) NOT NULL , type INT NOT NULL , PRIMARY KEY (id), UNIQUE (username)) ENGINE = InnoDB;
 
 create user appuser identified by "appuserPasswd";
 grant all privileges on test.* to appuser@'%';
